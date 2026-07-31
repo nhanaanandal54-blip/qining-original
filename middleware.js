368b1a896168e6db4fea70f524c9faacc09d6ecb
Qining <qining@example.com>
2026-07-31 21:58:57 +0800

    Use Next middleware for API proxy

--- middleware.js
+++ middleware.js
@@ -1,10 +1,12 @@
+import { NextResponse } from "next/server";
+
 const API_ORIGIN = "https://qining-original.vercel.app";
 
-export function middleware({ request, rewrite }) {
+export function middleware(request) {
   const sourceUrl = new URL(request.url);
   const targetUrl = new URL(sourceUrl.pathname + sourceUrl.search, API_ORIGIN);
 
-  return rewrite(targetUrl.toString());
+  return NextResponse.rewrite(targetUrl);
 }
 
 export const config = {
